ea849075be9f2fa1929d41626e3eca8e6df86c54
karynaamorim <25529777-karynaamorim@users.noreply.replit.com>
2023-08-24 00:46:08 +0000

    arquivos da aula 1

---
+++ aulas/aula01/meuprograma.c
@@ -0,0 +1,15 @@
+/*
+  autor: karina
+  data: 23/08/23
+
+  programa que exibe ola pessoal!
+  */
+#include <stdio.h>
+#include <math.h>
+
+// funcao pricipal
+int main(){
+  printf("ola pessoal!\n");
+  printf("a raiz quadrada de 2 é %f\n", sqrt(2));
+  return 0;
+}
